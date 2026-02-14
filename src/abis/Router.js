ccb44388b4f5a32c0278f8ce53a2c4bb2e188c2d
maduonyekaemeka33 <maduonyekaemeka@gmail.com>
2026-02-14 07:42:56 +0100

    Create Router.js

---
+++ src/abis/Router.js
@@ -0,0 +1,4 @@
+export const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"; // Uniswap V2
+export const ROUTER_ABI = [
+  "function swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 minAmountOut, uint256 deadline) external"
+];
