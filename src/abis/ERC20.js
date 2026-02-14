323ec71b7ebbe10d84febe7f06f4856ea8ba0e03
maduonyekaemeka33 <maduonyekaemeka@gmail.com>
2026-02-14 07:42:01 +0100

    Create ERC20.js

---
+++ src/abis/ERC20.js
@@ -0,0 +1,7 @@
+export const ERC20_ABI = [
+  "function approve(address spender, uint256 amount) external returns (bool)",
+  "function allowance(address owner, address spender) view returns (uint256)",
+  "function balanceOf(address owner) view returns (uint256)",
+  "function decimals() view returns (uint8)",
+  "function transferFrom(address sender, address recipient, uint256 amount) external returns (bool)"
+];
